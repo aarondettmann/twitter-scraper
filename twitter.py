ef6c1cc6a42c76396008d914fa26b02f25f0f09a
Aaron Dettmann <42786424+aarondettmann@users.noreply.github.com>
2020-04-03 15:50:42 +0200

    Write Twitter data to Excel file

--- twitter.py
+++ twitter.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import argparse
 import json
 import logging
+import openpyxl as xl
 import os
 import sys
 
@@ -41,7 +42,7 @@ def cli():
 
     # ----- Mode 'down' -----
     sub = subparsers.add_parser('down', help='download target twitter feed')
-    sub.add_argument('username', metavar='NAME', type=str, help='target twitter profile')
+    sub.add_argument('usernames', metavar='NAMES', nargs='+', type=str, help='target twitter profile')
     sub.add_argument('--pages', '-p', metavar='PAGES', type=int, help='number of pages to fetch', default=20)
 
     # ----- Mode plot -----
@@ -49,14 +50,25 @@ def cli():
     sub.add_argument("filename", metavar='FILE', help="data to visualize", type=str)
     sub.add_argument("--show", "-s", metavar='PLOTNAME', help="available plots: 'activity'", type=str, default='activity')
 
+    # ----- Mode 'xl' -----
+    sub = subparsers.add_parser('xl', help='convert data to excel spreadsheet')
+    sub.add_argument("filename", metavar='FILE', help="data to convert", type=str)
+
     args = parser.parse_args()
 
     if args.exec_mode == 'down':
-        download_history(args.username, args.pages)
+        for username in args.usernames:
+            download_history(username, args.pages)
     elif args.exec_mode == 'plot':
         filename = os.path.abspath(args.filename)
         twitter_data = load_twitter_data(filename)
         plot_tweet_activity(twitter_data)
+    elif args.exec_mode == 'xl':
+        json_file = os.path.abspath(args.filename)
+        excel_file = json_file.replace(".json", ".xlsx")
+
+        twitter_data = load_twitter_data(json_file)
+        convert_to_excel(twitter_data, excel_file)
     else:
         parser.print_help()
 
@@ -131,10 +143,7 @@ def plot_tweet_activity(twitter_data):
     profile = twitter_data['profile']
     username = profile['username']
 
-    tweets_per_day = Counter()
-    for tweet in tweets:
-        time = datetime.fromisoformat(tweet['time'])
-        tweets_per_day[datetime(time.year, time.month, time.day)] += 1
+    tweets_per_day = get_tweets_per_day(twitter_data)
 
     logging.info(f"Plotting tweet activity for {username}...")
     dates = list(tweets_per_day.keys())
@@ -146,6 +155,76 @@ def plot_tweet_activity(twitter_data):
     plt.xlabel("Time")
     plt.ylabel("Number of tweets")
     plt.show()
+
+
+def get_tweets_per_day(twitter_data):
+    """
+    Return tweets per day
+
+    Args:
+        :twitter_data: (dict) dictionary with twitter data
+
+    Returns:
+        :tweets_per_day: (dict) dictionary with day (datetime object) and tweet count
+    """
+
+    tweets = twitter_data['history']
+    tweets_per_day = Counter()
+    for tweet in tweets:
+        time = datetime.fromisoformat(tweet['time'])
+        tweets_per_day[datetime(time.year, time.month, time.day)] += 1
+    return tweets_per_day
+
+
+def convert_to_excel(twitter_data, excel_file):
+    """
+    Convert a twitter data dict to excel file
+
+    Args:
+        :twitter_data: (dict) dictionary with twitter data
+        :excel_file: (str) excel file name
+    """
+
+    logging.info("Creating excel file...")
+    workbook = xl.Workbook()
+
+    # ----- Tweet data -----
+    sheet1 = workbook.active
+    sheet1.title = "Tweet raw data"
+    tweets = twitter_data['history']
+
+    headers = ["Time", "isRetweet", "replies", "likes", "hashtags", "text"]
+    for i, header in enumerate(headers, start=1):
+        sheet1.cell(row=1, column=i, value=header)
+
+    for i, tweet in enumerate(tweets, start=2):
+        sheet1.cell(row=i, column=1, value=tweet['time'])
+        sheet1.cell(row=i, column=2, value=tweet['isRetweet'])
+        sheet1.cell(row=i, column=3, value=tweet['replies'])
+        sheet1.cell(row=i, column=4, value=tweet['likes'])
+        sheet1.cell(row=i, column=5, value=str(tweet['entries']['hashtags']))
+        sheet1.cell(row=i, column=6, value=tweet['text'])
+
+    # ----- Tweets per day -----
+    sheet2 = workbook.create_sheet(title="Twitter activity")
+    for i, header in enumerate(["Time", "numTweets"], start=1):
+        sheet2.cell(row=1, column=i, value=header)
+
+    tweets_per_day = get_tweets_per_day(twitter_data)
+    for i, (day, num_tweets) in enumerate(tweets_per_day.items(), start=2):
+        sheet2.cell(row=i, column=1, value=day)
+        sheet2.cell(row=i, column=2, value=num_tweets)
+
+    # ----- User data -----
+    sheet3 = workbook.create_sheet(title="Account")
+    profile = twitter_data['profile']
+    headers = ["name", "username", "likes_count", "tweets_count", "followers_count", "following_count"]
+    for i, header in enumerate(headers, start=1):
+        sheet3.cell(row=i, column=1, value=header)
+        sheet3.cell(row=i, column=2, value=profile[header])
+
+    logging.info(f"Saving data: {excel_file}...")
+    workbook.save(excel_file)
 
 
 if __name__ == '__main__':
