f6bd3c7008eb59819d07e6486a505178ef33f9e7
Aaron Dettmann <42786424+aarondettmann@users.noreply.github.com>
2020-04-08 11:00:46 +0200

    Add repo update script for Windows

---
+++ update.bat
@@ -0,0 +1,14 @@
+:: Turn off command echoing feature
+@echo off
+
+WHERE git >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO ERROR: please install git... Exit.
+	EXIT
+)
+
+:: Change into script directory
+CD %root% >nul
+
+ECHO INFO: updating twitter-scraper... & echo.
+git pull https://github.com/aarondettmann/twitter-scraper
